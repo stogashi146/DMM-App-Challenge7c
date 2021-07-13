1f96dc91d4fd0acab447f7a0ecd2835fbff68a89
stogashi146 <stogashi146@gmail.com>
2021-07-13 12:55:03 +0000

    [Update] グループ作成実装

--- app/controllers/user_groups_controller.rb
+++ app/controllers/user_groups_controller.rb
@@ -1,19 +1,26 @@
 class UserGroupsController < ApplicationController
   def index
     @groups = UserGroup.all
+    @memebers = UserGroupMember.all
     @book = Book.new
   end
 
   def new
     @group = UserGroup.new
-
   end
+
   def create
     @group = UserGroup.new(group_params)
-    current_user.user_group_members.user_id = current_user.id
-    current_user.user_group_members.is_orner = true
-    user_group.user_group_members.group_id = @group.id
+    member = @group.user_group_members.new
+    member.user_id = current_user.id
+    member.user_group_id = @group.id
+    member.is_orner = true
+    # current_user.user_group_members.user_id = current_user.id
+    # current_user.user_group_members.is_orner = true
+    # user_group.user_group_members.group_id = @group.id
     @group.save
+    member.save
+
     redirect_to user_groups_path
   end
 
--- app/models/user_group.rb
+++ app/models/user_group.rb
@@ -1,5 +1,5 @@
 class UserGroup < ApplicationRecord
   has_many :user_group_members
-  has_many :group_users,through: :user_group_members
+  has_many :users,through: :user_group_members
   attachment :group_image, destroy: false
 end
